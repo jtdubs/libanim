da891b8894134c6e825e87eea737888a9d48253a
Justin Dubs <jtdubs@gmail.com>
2009-04-22 17:55:29 -0400

    tweak example and readme

--- src/example.c
+++ src/example.c
@@ -6,25 +6,25 @@ float x=0, y=0, z=0, s1=0, s2=0;
 
 void show_animation(Animation* a, float step);
 
-float id(float x) {
-    return x * 2;
+float fcos(float x) {
+    return cos(x);
 }
 
 int main() {
-    Animation* a = parallelpn(sequencen(sinusoid(scale(linearf1(&x, 0, 4), 5)),
-                                        sinusoid(scale(linearf1(&x, 4, 2), 3)),
-                                        NULL),
-                              sequencen(delay(sinusoid(scale(linearf1(&y, 0, 4), 3)), 3),
-                                        NULL),
-                              sequencen(scale(attachn(linearf1(&z, 2, 1),
-                                                      derivef1(id, &z, &s1),
-                                                      derivef1(id, &s1, &s2),
-                                                      NULL),
-                                              3),
-                                        scale(linearf1(&z, 1, 3), 3),
-                                        scale(linearf1(&z, 3, 1), 2),
-                                        NULL),
-                              NULL);
+    Animation* a = attach(parallelpn(sequencen(sinusoid(scale(linearf1(&x, 0, 4), 5)),
+                                               sinusoid(scale(linearf1(&x, 4, 2), 3)),
+                                               NULL),
+                                     sequencen(delay(sinusoid(scale(linearf1(&y, 0, 4), 3)), 3),
+                                               NULL),
+                                     sequencen(scale(attachn(linearf1(&z, 2, 1),
+                                                             derivef1(fcos, &z, &s1),
+                                                             NULL),
+                                                     3),
+                                               scale(linearf1(&z, 1, 3), 3),
+                                               scale(linearf1(&z, 3, 1), 2),
+                                               NULL),
+                                     NULL),
+                          derivef1(fcos, &x, &s2));
     show_animation(a, 0.02);
     animation_free(a);
     return 0;
